70cb3cf743bbde24bbbbd0d1081656d6a7d811e0
Robert Loterh <rloterh@gmail.com>
2020-10-06 04:14:03 +0000

    Create function to populate scrape content to csv

--- bin/main.rb
+++ bin/main.rb
@@ -7,6 +7,10 @@ require 'Pry'
 require 'csv'
 require 'io/console'
 
+$post_game = ''
+$sub_path = ''
+$index_path = ''
+
 def scraper
   url = "https://apps.apple.com/us/genre/ios-games/id6014/#{$sub_path}#{$index_path}"
 
@@ -15,4 +19,24 @@ def scraper
 
   games_array = []
 
+  # parse the data
+  parsed_page.css('[id = "content"]').css('[id = "selectedgenre"]').css('[id = "selectedcontent"]').map do |a|
+    $post_game = a.text
+    games_array.push($post_game)
+  end
+
+   # clear and write content to csv
+   File.open('../lib/iOSGames.csv', 'w') {}
+   CSV.open('../lib/iOSGames.csv', 'w') do |csv|
+     csv << games_array
+   end
+   # Print CSV to console
+   CSV.foreach('iOSGames.csv') do |row|
+     puts row
+   end
+  
+  Pry.start(binding)
+
 end
+
+scraper
