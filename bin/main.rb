5ba1bfc1714f61391b1a30cd456d1cf3b666412f
Robert Loterh <rloterh@gmail.com>
2020-10-06 04:04:20 +0000

    Create a scraper method and parse scraper url

--- bin/main.rb
+++ bin/main.rb
@@ -6,3 +6,13 @@ require 'httparty'
 require 'Pry'
 require 'csv'
 require 'io/console'
+
+def scraper
+  url = "https://apps.apple.com/us/genre/ios-games/id6014/#{$sub_path}#{$index_path}"
+
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+
+  games_array = []
+
+end
