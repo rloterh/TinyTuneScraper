2e4ce78006982edd64598080be3e1f25f7b3e0e6
Robert Loterh <rloterh@gmail.com>
2020-10-12 02:40:29 +0000

    Move check_input method to Sraper class

--- bin/main.rb
+++ bin/main.rb
@@ -6,14 +6,14 @@ require 'Pry'
 require 'csv'
 require 'io/console'
 
-require_relative '../lib/Display.rb'
-require_relative '../lib/Scraper.rb'
+require_relative '../lib/display.rb'
+require_relative '../lib/scraper.rb'
 screen_o = Display.new
 scraper_o = Scraper.new
 
 screen_o.display_intro_screen
 
-screen_o.check_choice
+scraper_o.check_choice
 
 scraper_o.scrape
 
