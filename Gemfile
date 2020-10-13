643953287f4c2ff83cddf46d87b87cd18487ecf5
Robert Loterh <rloterh@gmail.com>
2020-10-13 08:55:29 +0000

    Move display_intro_screen to main.rb and update gitignore file

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+require 'rubygems'
+require 'bundler/setup'
+
+gem 'byebug'
+gem 'csv'
+gem 'httparty'
+gem 'nokogiri'
+gem 'rubocop'
--- bin/main.rb
+++ bin/main.rb
@@ -11,7 +11,33 @@ require_relative '../lib/scraper.rb'
 screen_o = Display.new
 scraper_o = Scraper.new
 
-screen_o.display_intro_screen
+def display_intro_screen
+  puts ''
+  puts "                                               T I N Y T U N E S    S C R A P E R\n"
+  puts '                                                                 . . .'
+  puts '                                                               . - - :'
+  puts '                                                            . - - :'
+  puts '                                                  . : : : : . : : : : : : : .'
+  puts '                                              : - - - - - - - - - - - - - : : .'
+  puts '                                             . - - - - - - - - - - - - - -:'
+  puts '                                             : - - - - - - - - - - - - - -:'
+  puts '                                             : - - - - - - - - - - - - - -:'
+  puts '                                             : - - - - - - - - - - - - - -:'
+  puts '                                              . - - - - - - - - - - - - - - - .'
+  puts '                                               . - - - - - - - - - - - - - - .'
+  puts '                                                 . - - - - - - - - - - - - .'
+  puts '                                                  . : : : . . . : : : : .'
+  puts "                                                     .  .  .     . . .\n\n"
+  puts '                                                           V..1.0 '
+  puts "                                          An iTunes App Store games Mini Scraping Tool\n\n\n"
+  puts '   HOW TO:-'
+  puts '   ========'
+  puts '     [Type  \'pop\', and press Enter/ Return key] -> To list POPULAR iTunes Appstore games'
+  puts '     [Type a letter, |A-Z|, and press Enter/ Return] -> To list the corresponding Alphabetical game index'
+  puts '     [Type  \'more\', and press Enter/ Return key] -> To list MORE iTunes Appstore games'
+end
+
+display_intro_screen
 
 scraper_o.check_choice
 
